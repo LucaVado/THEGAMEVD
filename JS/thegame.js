e0181064125f310227858d82d316eb5b643b9a79
LucaVado <Luca_Vado@hotmail.com>
2023-09-04 14:35:16 -0700

    Se agrego el html y js de convertir las cartas en Matriz para las cartas faltantes en este caso la fila superior e inferior las cartas 1 y 100 por lo tanto 4 cartas en total;Faltante Mazo y Mano

--- JS/thegame.js
+++ JS/thegame.js
@@ -1,17 +1,50 @@
 const filaSuperiorIzquierda = [
     "../IMG/GAME1.png"
 ]; // Una matriz que almacena las rutas de las imágenes en la carpeta "IMG".
-
+const filaSuperiorDerecha = [
+    "../IMG/GAME1.png"
+];
+const filaInferiorIzquierda = [
+    "../IMG/GAME100.png"
+];
+const filaInferiorDerecha = [
+    "../IMG/GAME100.png"
+];
 const currentSuperiorIzquierda = filaSuperiorIzquierda.length - 1; // El índice de la imagen actual, inicializado en la última imagen de la lista.
+const currentSuperiorDerecha = filaSuperiorDerecha.length - 1;
+const currentInferiorIzquierda = filaInferiorIzquierda.length - 1;
+const currentInferiorDerecha = filaInferiorDerecha.length - 1;
 
-const imageDisplay = document.getElementById("GAME1Izquierda"); // Obtiene el elemento de imagen por su ID.
+const imageDisplaySuperiorIzquierda = document.getElementById("GAME1Izquierda"); // Obtiene el elemento de imagen por su ID.
+const imageDisplaySuperiorDerecha = document.getElementById("GAME1Derecha");
+const imageDisplayInferiorIzquierda = document.getElementById("GAME100Izquierda");
+const imageDisplayInferiorDerecha = document.getElementById("GAME100Derecha");
 
 // Función que muestra la imagen actual en el elemento de imagen.
 function displayCurrentImage() {
+    //SuperiorIzquierda
     if (currentSuperiorIzquierda >= 0 && currentSuperiorIzquierda < filaSuperiorIzquierda.length) {
-        imageDisplay.src = filaSuperiorIzquierda[currentSuperiorIzquierda];
+        imageDisplaySuperiorIzquierda.src = filaSuperiorIzquierda[currentSuperiorIzquierda];
     } else {
-        imageDisplay.src = ""; // Si currentIndex está fuera del rango, muestra una imagen vacía.
+        imageDisplaySuperiorIzquierda.src = ""; // Si currentIndex está fuera del rango, muestra una imagen vacía.
+    }
+    //SuperiorDerecha
+    if (currentSuperiorDerecha >= 0 && currentSuperiorDerecha < filaSuperiorDerecha.length) {
+        imageDisplaySuperiorDerecha.src = filaSuperiorDerecha[currentSuperiorDerecha];
+    } else {
+        imageDisplaySuperiorDerecha.src = ""; // Si currentIndex está fuera del rango, muestra una imagen vacía.
+    }
+    //InferiorIZquierda
+    if (currentInferiorIzquierda >= 0 && currentInferiorIzquierda < filaInferiorIzquierda.length) {
+        imageDisplayInferiorIzquierda.src = filaInferiorIzquierda[currentInferiorIzquierda];
+    } else {
+        imageDisplayInferiorIzquierda.src = ""; // Si currentIndex está fuera del rango, muestra una imagen vacía.
+    }
+    //InferiorDerecha
+    if (currentInferiorDerecha >= 0 && currentInferiorDerecha < filaInferiorDerecha.length) {
+        imageDisplayInferiorDerecha.src = filaInferiorDerecha[currentInferiorDerecha];
+    } else {
+        imageDisplayInferiorDerecha.src = ""; // Si currentIndex está fuera del rango, muestra una imagen vacía.
     }
 }
 

448eae1cf6a2962828196fb69acd39b887ccea82
LucaPC <lucavadovd@gmail.com>
2023-10-24 17:32:05 -0700

    Se agregaron imagenes y contenido principal para la estructura el html, donde se utiliza la libreria gsap para el movimiento de scroll

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,80 @@
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+
+if (ScrollTrigger.isTouch !== 1) {
+
+    ScrollSmoother.create({
+        wrapper: '.wrapper',
+        content: '.content',
+        smooth: 1.5,
+        effects: true
+    })
+
+    gsap.fromTo('.hero-section', { opacity: 1 }, {
+        opacity: 0,
+        scrollTrigger: {
+            trigger: '.hero-section',
+            start: 'center',
+            end: '820',
+            scrub: true
+        }
+    })
+
+    let itemsL = gsap.utils.toArray('.gallery__left .gallery__item')
+
+    itemsL.forEach(item => {
+        gsap.fromTo(item, { opacity: 0, x: -50 }, {
+            opacity: 1, x: 0,
+            scrollTrigger: {
+                trigger: item,
+                start: '-850',
+                end: '-100',
+                scrub: true
+            }
+        })
+    })
+
+    let itemsR = gsap.utils.toArray('.gallery__right .gallery__item')
+
+    itemsR.forEach(item => {
+        gsap.fromTo(item, { opacity: 0, x: 50 }, {
+            opacity: 1, x: 0,
+            scrollTrigger: {
+                trigger: item,
+                start: '-750',
+                end: 'top',
+                scrub: true
+            }
+        })
+    })
+
+}
+
+// Obtén el botón y la sección de destino por su ID
+const botonIrASeccion = document.getElementById("botonIrASeccion");
+const seccionDestino = document.getElementById("seccionDestino");
+
+botonIrASeccion.addEventListener("click", function () {
+    const offset = seccionDestino.getBoundingClientRect().top + window.scrollY;
+    window.scrollTo({
+        top: offset,
+        behavior: "smooth"
+    });
+});
+
+
+
+const items = document.querySelectorAll(".accordion-item");
+items.forEach((item) => {
+    item.addEventListener("click", () => {
+        items.forEach(item => {
+            item.classList.remove("item-active");
+        });
+        item.classList.add("item-active");
+    });
+});
+
+//ANTERIOR
+
 document.addEventListener("DOMContentLoaded", function() {
     const verReglasButton = document.getElementById("verReglas");
     const jugarNuevaPartidaButton = document.getElementById("jugarNuevaPartida");

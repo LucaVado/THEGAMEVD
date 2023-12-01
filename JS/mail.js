4d8fba36bffbb93afbc5defee7e20b5fd2bd6558
LucaPC <lucavadovd@gmail.com>
2023-12-01 02:41:17 -0700

    Se ajusto el cambio de titulo

--- JS/mail.js
+++ JS/mail.js
@@ -1,12 +1,18 @@
 import { rankRef } from './firebaseConfig';
 import { rankRefE } from './firebaseConfig';
-let isRankRefDisplayed = true;
+let isRankRefDisplayed = false;
+let isRankRefDisplayed1 = false;
 
-//....
+window.addEventListener('load', toggleTables);
 // Función para cambiar entre tablas
 function toggleTables() {
     isRankRefDisplayed = !isRankRefDisplayed; // Cambia el estado de la variable
-
+    isRankRefDisplayed1 = !isRankRefDisplayed1; // Cambia el estado de la variable
+    // Cambiar el título
+    const titulo = document.getElementById('tituloTabla');
+    const titulo1 = document.getElementById('tituloTabla1');
+    titulo.innerHTML = isRankRefDisplayed ? 'Mejores puntuaciones' : 'Mejores puntuaciones extremos!';
+    titulo1.innerHTML = isRankRefDisplayed1 ? 'Mejores puntuaciones' : 'Mejores puntuaciones extremos!';
     if (isRankRefDisplayed) {
         // Muestra la tabla 'rankRef'
         rankRef.on('value', (snapshot) => {
@@ -115,72 +121,124 @@ function toggleTables() {
             }
         });
     }
+
+    if (isRankRefDisplayed1) {
+        // Muestra la tabla 'rankRef'
+        rankRef.on('value', (snapshot) => {
+            const rankDataCompleta = snapshot.val();
+
+            // Convierte los datos en un arreglo de objetos
+            const rankArray = [];
+            for (const key in rankDataCompleta) {
+                rankArray.push({
+                    id: key,
+                    nombre: rankDataCompleta[key].nombre,
+                    score: rankDataCompleta[key].score,
+                    seed: rankDataCompleta[key].seed,
+                    vecesjugada: rankDataCompleta[key].vecesjugada,
+                });
+            }
+
+            // Ordena el arreglo por el puntaje de menor a mayor donde ordenada score y luego vecesjugada
+            rankArray.sort((a, b) => {
+                if (a.score === b.score) {
+                    return a.vecesjugada - b.vecesjugada;
+                }
+                return a.score - b.score;
+            });
+
+            const puntuacionesTableCompleta = document.getElementById('puntuacionesTableCompleta');
+            const tbodyCompleta = puntuacionesTableCompleta.querySelector('tbody');
+
+            // Borra las filas existentes en la tabla
+            tbodyCompleta.innerHTML = '';
+
+            // Recorre los datos ordenados y crea filas para la tabla
+            let rank = 1;
+            for (const entry of rankArray) {
+                const usuario = entry.nombre;
+                const puntuaje = entry.score;
+                const vecesjugada = entry.vecesjugada;
+                const seed = entry.seed;
+
+                const row = tbodyCompleta.insertRow();
+                const rankCell = row.insertCell(0);
+                const puntuajeCell = row.insertCell(1);
+                const usuarioCell = row.insertCell(2);
+                const vecesjugadaCell = row.insertCell(3);
+
+                const seedCell = row.insertCell(4);
+
+                rankCell.innerHTML = `${rank}th`;
+                puntuajeCell.innerHTML = puntuaje;
+                usuarioCell.innerHTML = usuario;
+                vecesjugadaCell.innerHTML = vecesjugada;
+                seedCell.innerHTML = seed;
+
+                rank++; // Incrementa el rango
+            }
+        });
+    } else {
+        // Muestra la tabla 'rankRefE'
+        rankRefE.on('value', (snapshot) => {
+            const rankDataCompleta = snapshot.val();
+
+            // Convierte los datos en un arreglo de objetos
+            const rankArray = [];
+            for (const key in rankDataCompleta) {
+                rankArray.push({
+                    id: key,
+                    nombre: rankDataCompleta[key].nombre,
+                    score: rankDataCompleta[key].score,
+                    seed: rankDataCompleta[key].seed,
+                    vecesjugada: rankDataCompleta[key].vecesjugada,
+                });
+            }
+
+            // Ordena el arreglo por el puntaje de menor a mayor donde ordenada score y luego vecesjugada
+            rankArray.sort((a, b) => {
+                if (a.score === b.score) {
+                    return a.vecesjugada - b.vecesjugada;
+                }
+                return a.score - b.score;
+            });
+
+            const puntuacionesTableCompleta = document.getElementById('puntuacionesTableCompleta');
+            const tbodyCompleta = puntuacionesTableCompleta.querySelector('tbody');
+
+            // Borra las filas existentes en la tabla
+            tbodyCompleta.innerHTML = '';
+
+            // Recorre los datos ordenados y crea filas para la tabla
+            let rank = 1;
+            for (const entry of rankArray) {
+                const usuario = entry.nombre;
+                const puntuaje = entry.score;
+                const vecesjugada = entry.vecesjugada;
+                const seed = entry.seed;
+
+                const row = tbodyCompleta.insertRow();
+                const rankCell = row.insertCell(0);
+                const puntuajeCell = row.insertCell(1);
+                const usuarioCell = row.insertCell(2);
+                const vecesjugadaCell = row.insertCell(3);
+
+                const seedCell = row.insertCell(4);
+
+                rankCell.innerHTML = `${rank}th`;
+                puntuajeCell.innerHTML = puntuaje;
+                usuarioCell.innerHTML = usuario;
+                vecesjugadaCell.innerHTML = vecesjugada;
+                seedCell.innerHTML = seed;
+
+                rank++; // Incrementa el rango
+            }
+        });
+    }
 }
 
 // Agrega un evento al botón para cambiar entre las tablas
 const btnToggleTables = document.getElementById('btnToggleTables');
 btnToggleTables.addEventListener('click', toggleTables);
-
-
-
-
-//....
-
-if(document.getElementById('puntuacionesTableCompleta') !=null) {
-    rankRef.on('value', (snapshot) => {
-        const rankDataCompleta = snapshot.val();
-
-        // Convierte los datos en un arreglo de objetos
-        const rankArray = [];
-        for (const key in rankDataCompleta) {
-            rankArray.push({
-                id: key,
-                nombre: rankDataCompleta[key].nombre,
-                score: rankDataCompleta[key].score,
-                seed: rankDataCompleta[key].seed,
-                vecesjugada: rankDataCompleta[key].vecesjugada,
-            });
-        }
-
-        // Ordena el arreglo por el puntaje de menor a mayor donde ordenada score y luego vecesjugada
-        rankArray.sort((a, b) => {
-            if (a.score === b.score) {
-                return a.vecesjugada - b.vecesjugada;
-            }
-            return a.score - b.score;
-        });
-
-        const puntuacionesTableCompleta = document.getElementById('puntuacionesTableCompleta');
-        const tbodyCompleta = puntuacionesTableCompleta.querySelector('tbody');
-
-        // Borra las filas existentes en la tabla
-        tbodyCompleta.innerHTML = '';
-
-        // Recorre los datos ordenados y crea filas para la tabla
-        let rank = 1;
-        for (const entry of rankArray) {
-            const usuario = entry.nombre;
-            const puntuaje = entry.score;
-            const vecesjugada = entry.vecesjugada;
-            const seed = entry.seed;
-
-            const row = tbodyCompleta.insertRow();
-            const rankCell = row.insertCell(0);
-            const puntuajeCell = row.insertCell(1);
-            const usuarioCell = row.insertCell(2);
-            const vecesjugadaCell = row.insertCell(3);
-
-            const seedCell = row.insertCell(4);
-
-            rankCell.innerHTML = `${rank}th`;
-            puntuajeCell.innerHTML = puntuaje;
-            usuarioCell.innerHTML = usuario;
-            vecesjugadaCell.innerHTML = vecesjugada;
-            seedCell.innerHTML = seed;
-
-            rank++; // Incrementa el rango
-        }
-
-
-    });
-}
+const btnToggleTables1 = document.getElementById('btnToggleTables1');
+btnToggleTables1.addEventListener('click', toggleTables);
